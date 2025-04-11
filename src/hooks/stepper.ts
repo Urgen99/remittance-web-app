642bcb99a69e4effb02d612cd03c9796a93b380c
SanzayKdg <sanjaykhadgi9861@gmail.com>
2025-04-11 14:57:44 +0545

    fixes

--- src/hooks/stepper.ts
+++ src/hooks/stepper.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 
 const useStepper = (page: string) => {
   const [activeStep, setActiveStep] = useState(page || "user-details");
@@ -11,35 +11,6 @@ const useStepper = (page: string) => {
     handleNext,
     handlePrev,
   };
-};
-
-export const useSteppers = <T extends string | number>(
-  steps: T[],
-  initialStep: T
-) => {
-  const [activeStep, setActiveStep] = useState<T>(initialStep);
-  const prevStepRef = useRef<T>(initialStep);
-  const [delta, setDelta] = useState(0);
-
-  useEffect(() => {
-    const prevIndex = steps.indexOf(prevStepRef.current);
-    const currentIndex = steps.indexOf(activeStep);
-    setDelta(currentIndex - prevIndex);
-    prevStepRef.current = activeStep;
-  }, [activeStep, steps]);
-
-  const handleNext = () => {
-    const index = steps.indexOf(activeStep);
-    if (index < steps.length - 1) setActiveStep(steps[index + 1]);
-  };
-
-  const handlePrev = () => {
-    const index = steps.indexOf(activeStep);
-    if (index > 0) setActiveStep(steps[index - 1]);
-  };
-
-  const progress = (steps.indexOf(activeStep) / (steps.length - 1)) * 100;
-  return { activeStep, setActiveStep, handleNext, handlePrev, delta, progress };
 };
 
 export default useStepper;
